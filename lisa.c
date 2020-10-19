4ce0d61191a1b3f4700d78f06b082071ccd8649e
wintersoldie <73120282+wintersoldie@users.noreply.github.com>
2020-10-19 21:39:41 +0530

    Add files via upload

---
+++ lisa.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int min(int arr[],int a,int b){
+    int l=arr[a];
+    for(int i=a;i<=b;i++){
+        if(l>arr[i])
+        l=arr[i];
+    }
+    return l;
+}
+int main (){
+    int n,t,a,b;
+    scanf("%d%d",&n,&t);
+    int arr[n],frr[t];
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(int i=0;i<t;i++){
+        scanf("%d %d",&a,&b);
+        frr[i]=min(arr,a,b);
+    }
+
+}
